64b03e72d4cc3759b00a9fcdda0fe529d522d18e
Tristan Guezennec <121152666+TriGuez@users.noreply.github.com>
2025-06-18 18:03:37 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 from source import BPModel
 from tools import *
 import numpy as np
+import scipy.sparse.linalg as spla
+import scipy.sparse as sp
+import matplotlib.pyplot as plt
 
 def StepIndexRI(X, Y, P):
     nClad = P.n0
@@ -12,15 +15,16 @@ def StepIndexRI(X, Y, P):
 P = BPModel()
 P.xmax = 200e-6
 P.ymax = 200e-6
-P.N = 200
+P.N = 300
 P.Lz = 1e-2
 P.dz = 1e-4
 P.n0 = silica_index(P.lambda0)
 P.set_RI_from_function(StepIndexRI)
 
-P.solve_modes(Nmodes=4, plotModes=False)
+P.solve_modes(Nmodes=40, plotModes=False)
 
-P.set_initial_field_from_mode(0, combination=False)
+P.set_initial_field_from_mode('all', combination=True)
 P.shift_field(0,0)
 P.show_field()
-P.propagate_full(animate=False)
+
+P.propagate_full(animate = True)
